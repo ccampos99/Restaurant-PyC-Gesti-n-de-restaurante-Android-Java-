b3c90e98a81c4c6b9d7e2a435a4c3ede4a4bb68c
PeterKUKURELO <gabrielkukurelo@gmail.com>
2025-07-05 09:25:26 -0500

    Tengo sueño x2

--- app/src/main/java/com/peter/restauranteproyecto/cashier/CashierActivity.java
+++ app/src/main/java/com/peter/restauranteproyecto/cashier/CashierActivity.java
@@ -1,4 +1,21 @@
 package com.peter.restauranteproyecto.cashier;
 
-public class CashierActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.peter.restauranteproyecto.R;
+import com.peter.restauranteproyecto.cashier.ui.fragment.CajaFragment;
+
+public class CashierActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cajero_main); // Lo veremos abajo
+
+        // Cargar CajaFragment en el contenedor
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container_cashier, new CajaFragment());
+        transaction.commit();
+    }
 }
